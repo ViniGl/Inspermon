09eadf0e1a6e1fb44514aa2ff4c8fa6feca86d48
Vinicius Lima <Vinicius Lima>
2017-03-30 10:03:56 -0300

    'Bla'

---
+++ Lista pokemon.py
@@ -0,0 +1,5 @@
+#Lista stats de cada pokemon
+#Ordem:Hp[0], Atk[1], Def[2], Sp atk[3], sp def[4], speed[5]
+pkm1=[45,49,49,65,65,45]
+pkm2=[]
+pkm3=[]
---
+++ lutas.py
@@ -0,0 +1,4 @@
+# ordem: Hp, Atk, Def, Sp. atk, Sp. def, speed
+pkm1=[]
+pkm2=[]
+pkm3=[] 
