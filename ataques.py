fdac11aff4ce61a9768140185d6b47716e2aa0dd
Vinicius Lima <Vinicius Lima>
2017-03-31 18:07:46 -0300

    a

---
+++ ataques.py
@@ -0,0 +1,33 @@
+#Grama, Fogo, Agua, Luta, normal, eletrico
+def ataques():
+	#Grama
+	vine_whip=35#0	
+	leaf_blade=70#1
+	bullet_seed=10#2
+	razor_leaf=55#3
+	#Luta
+	jump_kick=70#4
+	rock_smash=20#5
+	triple_kick=30#6
+	brick_break=75#7
+	#Fogo
+	ember=40#8
+	fire_spin=15#9
+	flame_wheel=60#10
+	fire_punch=75#11
+	#Agua
+	bubble=20#12
+	dive=60#13
+	water_pulse=60#14
+	bubblebeam=65#15
+	#Eletrico
+	thunder_bolt=95#16
+	spark=65#17
+	shock_wave=60#18
+	thunderpunch=75#19
+	#Normal
+	cut=50#20
+	egg_bomb=100#21
+	mega_punch=80#22
+	quick_attack=40#23
+	return vine_whip,leaf_blade,bullet_seed,razor_leaf,jump_kick,rock_smash,triple_kick,brick_break,ember,fire_spin,flame_wheel,fire_punch,bubble,dive,water_pulse,bubblebeam,thunder_bolt,spark,shock_wave,thunderpunch,cut,egg_bomb,mega_punch,quick_attack
