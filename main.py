c0cdc713beec56419ad6697087bd09dd5e120d2b
Vinicius Lima <Vinicius Lima>
2017-03-31 18:04:06 -0300

    a

--- main.py
+++ main.py
@@ -1,41 +1,49 @@
 # from ____ import reset_save
 # jogar=int(input("Se quiser jogar digite 0 se quiser resetar 1"))
 # reset_save(jogar)
-from lista_de_pkm import pokemon
+from lista_de_pkm import pokemon as pkm
 from lutas import engine_luta
-pokemons_capturados=[pokemon[0],pokemon[1],pokemon[2]]
+import os 
+from lutas import pokemon_lutador
+
+os.system('cls')
+pokemons_capturados=[pkm[0],pkm[3],pkm[6]]
 x=1
 escolhab=False
 pokemonb=False
 while escolhab==False:
 	oquefazer=input("O que voce deseja fazer, dormir ou andar?")
+	os.system('cls')
 	escolha=oquefazer.lower()
 	if escolha == "andar":
 		for i in pokemons_capturados:
 			print(i[0] + " ""(""{}""" ")".format(x))
 			x+=1
 		while pokemonb==False:
-			a=int(input("Escolha seu Pokemon:  "))
-			if a<= len(pokemons_capturados):
-				if a==1:
-					print("{} foi escolhido".format(pokemons_capturados[a-1][0]))
+			pokemon_escolhido=int(input("Escolha seu Pokemon: "))
+			a=list(pokemons_capturados[pokemon_escolhido-1])
+			pokemon_lutador(a)
+			os.system('cls')
+			if pokemon_escolhido<= len(pokemons_capturados):
+				if pokemon_escolhido==1:
+					print("{} foi escolhido".format(pokemons_capturados[pokemon_escolhido-1][0]))
 					pokemonb=True
-				elif a==2:
-					print("{} foi escolhido".format(pokemons_capturados[a-1][0]))
+				elif pokemon_escolhido==2:
+					print("{} foi escolhido".format(pokemons_capturados[pokemon_escolhido-1][0]))
 					pokemonb=True
-				elif a==3:
-					print("{} foi escolhido".format(pokemons_capturados[a-1][0]))
+				elif pokemon_escolhido==3:
+					print("{} foi escolhido".format(pokemons_capturados[pokemon_escolhido-1][0]))
 					pokemonb=True
-				elif a==4:
-					print("{} foi escolhido".format(pokemons_capturados[a-1][0]))
+				elif pokemon_escolhido==4:
+					print("{} foi escolhido".format(pokemons_capturados[pokemon_escolhido-1][0]))
 					pokemonb=True
-				elif a==5:
-					print("{} foi escolhido".format(pokemons_capturados[a-1][0]))
+				elif pokemon_escolhido==5:
+					print("{} foi escolhido".format(pokemons_capturados[pokemon_escolhido-1][0]))
 					pokemonb=True
-				elif a==6:
-					print("{} foi escolhido".format(pokemons_capturados[a-1][0]))
+				elif pokemon_escolhido==6:
+					print("{} foi escolhido".format(pokemons_capturados[pokemon_escolhido-1][0]))
 					pokemonb=True
-				engine_luta()
+				engine_luta(pokemon_escolhido)
 			else:
 				pokemonb=False
 		escolhab=True
