6e4be4c104410f642e8aaf77314b0169715895ac
gabrielvf1 <vf_gabriel@yahoo.com.br>
2017-04-20 23:50:15 -0300

    'MudeiMuito'

--- main.py
+++ main.py
@@ -12,45 +12,57 @@ import pickle
 import pygame
 from tkinter import *
 from Levelling import level_up
+from FuncaoSave import Save12
+from FuncaoSave import LendoSave
+
 #Pickle vida temp
 #Saves
 os.system('cls')
-class nomes_salvos:
-	jogadores_totais=0
-	def jogadores(self,jogadores):
-		self.jogadores=jogadores
-		nomes_salvos.jogadores_totais+=1
+# class nomes_salvos:
+# 	jogadores_totais=0
+# 	def jogadores(self,jogadores):
+# 		self.jogadores=jogadores
+# 		nomes_salvos.jogadores_totais+=1
 
 #musica		
-file = 'pkmsong1.mp3'
+# file = 'pkmsong1.mp3'
 file2='pkmfight.mp3'
-root = Tk()
-pygame.mixer.init()
-pygame.mixer.music.load(file)
-pygame.mixer.music.play()
-root.mainloop()
+# root = Tk()
+# pygame.mixer.init()
+# pygame.mixer.music.load(file)
+# pygame.mixer.music.play()
+# root.mainloop()
 
-# nome_jogador=input("Qual o seu nome treinador pokemon? ")
-# nome_jogador1=nome_jogador.title()
-# os.system('cls')
-# print("Seja Bem Vindo {}, pronto para se tornar o treinador pokemon mais respeitado da historia?".format(nome_jogador1))
-# time.sleep(4)
-# os.system('cls')
-# print("Agora vamos comecar a nossa dura jornada pelo mundo dos InsperMons!")
-# time.sleep(4)
-# os.system('cls')
-print ('{}  (1)\n{}  (2)\n{}  (3)' .format(pkm[0][0],pkm[3][0],pkm[6][0]))
-pokemon_inicial=int(input('Escolha seu primeiro pokemon:'))
-if pokemon_inicial==1:
-	pokemon_inicial=0
-elif pokemon_inicial==2:
-	pokemon_inicial=3
-elif pokemon_inicial==3:
-	pokemon_inicial=6
-inventario.pokemons_capturados.append(pkm[pokemon_inicial])
-inventario.pokebolas=7
-os.system('cls')
-
+inventario()
+# fazeroq=int(input("Continuar jogo salvo (1) ou comecar novo jogo(2)?"))
+# if fazeroq==2:
+# 	os.remove('nome.json')
+# 	nome_jogador=input("Qual o seu nome treinador pokemon? ")
+# 	nome_jogador1=nome_jogador.title()
+# 	os.system('cls')
+# 	print("Seja Bem Vindo {}, pronto para se tornar o treinador pokemon mais respeitado da historia?".format(nome_jogador1))
+# 	time.sleep(4)
+# 	os.system('cls')
+# 	print("Agora vamos comecar a nossa dura jornada pelo mundo dos InsperMons!")
+# 	time.sleep(4)
+# 	os.system('cls')
+# 	print ('{}  (1)\n{}  (2)\n{}  (3)' .format(pkm[0][0],pkm[3][0],pkm[6][0]))
+# 	pokemon_inicial=int(input('Escolha seu primeiro pokemon:'))
+# 	if pokemon_inicial==1:
+# 		pokemon_inicial=0
+# 	elif pokemon_inicial==2:
+# 		pokemon_inicial=3
+# 	elif pokemon_inicial==3:
+# 		pokemon_inicial=6
+# 	pokemons_capturados=[]
+# 	inventario.pokemons_capturados.append(pkm[pokemon_inicial])
+# 	Save12(inventario.pokemons_capturados)
+# 	inventario.pokebolas=7
+# 	os.system('cls')
+# if fazeroq==1:
+# 	inventario.pokemons_capturados.append(LendoSave())
+# 	print("Seja Bem Vindo de Volta")
+# 	time.sleep(2)
 #Loop da parte principal
 x=1
 escolhab=False
